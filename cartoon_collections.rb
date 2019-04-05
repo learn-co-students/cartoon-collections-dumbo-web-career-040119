f69de0beee7412188337b76475944d1ef672d78c
Codey Pinero <codey.pinero@gmail.com>
2019-04-05 01:34:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,17 @@
 def roll_call_dwarves(dwarves)
-    dwarves.each_with_index do |dwarf, index|
-      puts "#{index + 1}. #{dwarf}"
-    end
+    dwarves.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
 def summon_captain_planet(planeteer_calls)
-    planeteer_calls.map do |call|
-      call.capitalize << '!'
-    end
+    planeteer_calls.map { |call| call.capitalize << '!' }
 end
 
 def long_planeteer_calls(planeteer_calls)
-    planeteer_calls.any? { |word| word.length > 4 ? true : false }
+    planeteer_calls.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese(cheese_strings)
     cheese_types = ["cheddar", "gouda", "camembert"]
 
-    cheese_strings.find do |cheese_please|
-    cheese_types.include?(cheese_please)
-    end
+    cheese_strings.find { |cheese_please| cheese_types.include?(cheese_please) }
 end
